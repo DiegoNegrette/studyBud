eb4905d40b8ae5e40b0efdb6825a46bbc8a66e52
Diego Negrette <diegonegrette42@gmail.com>
2022-02-19 20:08:47 -0400

    Ultimo commit antes de la instalacion del tema

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('logout', views.logoutUser, name="logout"),
-    path('loging', views.loginPage, name="login"),
+urlpatterns = [    
+    path('loging/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     path('', views.home, name="home"), #core url, cuando alguien visita el sitio directamente
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
 ]
